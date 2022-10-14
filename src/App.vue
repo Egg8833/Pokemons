b37f54ca391bd5a237521c5b93c8df50f58dc1b5
黃禎平 <egg8833@gmail.com>
2022-10-14 10:27:54 +0800

    'update'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-12">
-    <router-link class="flex justify-center text-4xl text-yellow-700" to="/"
+    <router-link class="flex justify-center text-4xl text-yellow-700" to="/home"
       >Pokemon Picker</router-link
     >
   </div>
